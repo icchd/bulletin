bfc537dcbee0402768476d027174d304564b61c7
Savio Dimatteo <darksmo@gmail.com>
2016-12-09 00:33:12 +0100

    various improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 var m = function () { return moment.apply(this, arguments).locale("en-gb"); }
 
 var markdown = new Remarkable();
@@ -9,6 +10,42 @@ function getNextSunday(sFormat) {
     }
     return m().add(days, "days").format(sFormat || "LL");
 }
+
+// http://youmightnotneedjquery.com/
+function getJson(sUrl) {
+    if (!app._urlCache) {
+        app._urlCache = {};
+    }
+    if (app._urlCache[sUrl]) {
+        return new Promise(function (fnResolve) {
+            fnResolve(app._urlCache[sUrl]);
+        });
+    }
+    return new Promise(function (fnResolve, fnReject) {
+        var request = new XMLHttpRequest();
+        request.open('GET', sUrl, true);
+
+        request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                // Success!
+                var data = JSON.parse(request.responseText);
+                app._urlCache[sUrl] = data;
+                fnResolve(data);
+            } else {
+                // We reached our target server, but it returned an error
+                fnReject();
+            }
+        };
+
+        request.onerror = function() {
+            // There was a connection error of some sort
+            fnReject();
+        };
+
+        request.send();
+    });
+}
+
 
 var oBooks = {
     abbrev: {
@@ -179,6 +216,46 @@ function formatAppointment(vAppointment) {
 var app = new Vue({
     el: "#app",
     data: {
+        themes: {
+            White: {
+                color1: "#FFF",
+                color2: "#333",
+                border: "#333"
+            },
+            Red: {
+                color1: "#9D0000",
+                color2: "#FFF",
+                border: "#FFF"
+            },
+            Green: {
+                color1: "#3BBD00",
+                color2: "#FFF",
+                border: "#FFF"
+            },
+            Violet: {
+                color1: "#9B00BD",
+                color2: "#FFF",
+                border: "#FFF"
+            },
+            Black: {
+                color1: "#000",
+                color2: "#FFF",
+                border: "#FFF"
+            },
+            Rose: {
+                color1: "#FFCAEA",
+                color2: "#000",
+                border: "#FFF"
+            },
+            Gold: {
+                color1: "#CECA2F",
+                color2: "#000",
+                border: "#FFF"
+            }
+        },
+        toolbar: {
+            current: "color"
+        },
         colors: {
           hex: '#194d33',
           hsl: {
@@ -208,7 +285,9 @@ var app = new Vue({
             dateChanged: m().format("YYYY-MM-DD hh:mm:ss +02:00"),
             image: {
                 enabled: true,
-                src: "https://placeholdit.imgix.net/~text?txtsize=33&txt=Image&w=230&h=230"
+                src: "https://placeholdit.imgix.net/~text?txtsize=33&txt=Image&w=230&h=230",
+                layout: "left",
+                size: 50
             },
             title: "Easter Sunday",
             text: "**Lorem ipsum** dolor sit amet, consectetur adipiscing elit. Curabitur ultricies quam vel elit pulvinar, ac iaculis neque maximus. Nulla vitae sapien metus. Sed bibendum vestibulum elementum. Nullam sagittis in nunc id condimentum. Ut semper interdum mi. Aenean et purus nec tortor convallis porttitor eu non nunc. Nulla facilisi. Maecenas tempor ultricies quam sed imperdiet. Cras fringilla, ante in convallis viverra, urna massa lacinia massa, at rhoncus erat quam at est. Proin ut tincidunt erat. Ut mollis consequat elit, vitae laoreet justo hendrerit sit amet. Proin volutpat venenatis leo at tristique. Aenean ac ornare erat, nec euismod odio. Proin tempor accumsan gravida. Suspendisse sit amet consequat sapien.  Morbi euismod commodo massa, vel elementum urna tincidunt sed. Suspendisse cursus, nunc et imperdiet laoreet, elit erat efficitur diam, ut convallis est purus ac augue. Pellentesque eu dolor aliquet, sodales nisl efficitur, dignissim nisl. Morbi vel ultricies dolor. In facilisis condimentum ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean volutpat mauris id mi lobortis mattis. Suspendisse eget dapibus odio, eu luctus mauris. Donec vitae cursus ex. Suspendisse id interdum nisl. Fusce vulputate nibh sapien, sed cursus mi euismod vitae.  Nunc mauris mi, placerat eu sem congue, euismod faucibus tellus. Cras pretium augue nunc, sed elementum dui varius et. Proin ut tempor felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aliquam dictum, velit in suscipit fermentum, leo ligula laoreet dolor, eu semper lorem risus ut sapien. Nunc a accumsan velit, nec tristique augue. Vivamus viverra ipsum est, eu elementum est tempor at. Praesent cursus nunc vel lorem viverra, non luctus lectus lacinia. In at molestie velit, malesuada pretium ex.  Pellentesque eget erat pretium, pretium nulla in, dictum lectus. Aliquam eleifend posuere orci, ac scelerisque leo scelerisque sit amet. Vestibulum ultricies luctus rutrum. Vivamus sit amet vestibulum ex. In gravida magna felis, vel tempus leo condimentum quis. Quisque tellus erat, rhoncus vel euismod at, faucibus at orci. Mauris elementum condimentum odio, id hendrerit lorem blandit sit amet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam massa quam, consequat vel condimentum et, ultrices a tellus. Donec nisi nunc, ullamcorper vitae diam at, maximus imperdiet magna. Suspendisse potenti. Phasellus id ligula enim. Integer molestie rhoncus semper.  Suspendisse potenti. Vestibulum quis lectus sagittis, efficitur erat quis, efficitur quam. Morbi interdum commodo nisi ac iaculis. Vivamus nec viverra lacus. Nam tempor, dui non dapibus sagittis, diam risus porttitor felis, ac elementum est dui ac dolor. Nullam viverra magna quis augue cursus, id pulvinar ex fermentum. Vestibulum at cursus magna. Mauris lobortis arcu in urna placerat, suscipit fermentum nisl imperdiet. Vivamus sit amet odio sed lectus malesuada ultricies eu a augue. Nulla rutrum interdum purus eget vestibulum. Proin a est erat. Nulla ac metus sed justo suscipit ornare quis elementum ex. Fusce cursus molestie libero, id tincidunt eros venenatis ac.",
@@ -219,7 +298,9 @@ var app = new Vue({
             reading3: "Gospel",
             appointments: [ ],
             colors: {
-               color1: "000",
+               color1: "#000",
+               color2: "#FFF",
+               border: "#FFF"
             },
             fonts: {
                 text: {
@@ -234,17 +315,60 @@ var app = new Vue({
       'slider-picker': VueColor.Slider
     },
     methods: {
+        selectTab: function (sTabName) {
+            app.toolbar.current = sTabName;
+        },
         updateColor1: function (oColor) {
             app.bullettin.colors.color1 = oColor.hex;
         },
+        updateColor2: function (oColor) {
+            app.bullettin.colors.color2 = oColor.hex;
+        },
         toggleImage: function () {
-            app.bullettin.image.enabled = !app.bullettin.image.enabled;
+            var bEnabled = app.bullettin.image.enabled;
+            var sLayout = app.bullettin.image.layout;
+            
+            if (bEnabled) {
+                switch (sLayout) {
+                    case "left":
+                        app.bullettin.image.layout = "right";
+                        break;
+                    case "right":
+                        app.bullettin.image.layout = "left";
+                        app.bullettin.image.enabled = false;
+                        break;
+                }
+            } else {
+                app.bullettin.image.enabled = true;
+            }
+        },
+        increaseImageWidth: function () {
+            if (app.bullettin.image.size === 0) {
+                return;
+            }
+            app.bullettin.image.size -= 2.5;
+        },
+        decreaseImageWidth: function () {
+            if (app.bullettin.image.size === 100) {
+                return;
+            }
+            app.bullettin.image.size += 2.5;
         },
         increaseFontSize: function () {
             app.bullettin.fonts.text.size+=0.5;
         },
         decreaseFontSize: function () {
-            app.bullettin.fonts.text.size--;
+            if (app.bullettin.fonts.text.size > 1) {
+                app.bullettin.fonts.text.size-=0.5;
+            }
+        },
+        increaseLineHeight: function () {
+            app.bullettin.fonts.text.lineHeight+=0.5;
+        },
+        decreaseLineHeight: function () {
+            if (app.bullettin.fonts.text.lineHeight > 1) {
+                app.bullettin.fonts.text.lineHeight-=0.5;
+            }
         },
         publishBullettin: function () {
             // first make sure the bullettin is published
@@ -323,6 +447,105 @@ var app = new Vue({
         },
         md2html: function (sMarkdown) {
             return markdown.render(sMarkdown);
+        },
+        setTheme: function (sThemeName) {
+            if (!app.themes.hasOwnProperty(sThemeName)) {
+                alert.show("error", sThemeName + " is not a valid theme");
+            } else {
+                app.bullettin.colors.color1 = app.themes[sThemeName].color1;
+                app.bullettin.colors.color2 = app.themes[sThemeName].color2;
+                app.bullettin.colors.border = app.themes[sThemeName].border;
+            }
+        },
+        updateReadingFromSunday: function () {
+            function getReading(oResponse, sType) {
+                return oResponse.ReadingGroups[0].Readings.filter(
+                    function (oReading) { return oReading.Type === sType; }
+                )[0];
+            }
+            function abbrevReading(sReading) {
+                var aReading = sReading.split(" ");
+                var sBookName = aReading.shift();
+                while (aReading.length) {
+                    if (oBooks.abbrev.hasOwnProperty(sBookName)) {
+                        return oBooks.abbrev[sBookName] + " " + aReading.join(" ");
+                    }
+                    sBookName += " " + aReading.shift();
+                }
+
+                if (aReading.length === 0) {
+                    // return original
+                    return sReading;
+                }
+            }
+            var sSelectedSunday = m(app.bullettin.date, "D MMMM YYYY").format("YYYY-MM-DD");
+            getJson('https://www.ewtn.com/se/readings/readingsservice.svc/day/' + sSelectedSunday + '/en')
+                .then(function (oResponse) {
+
+                    try {
+                        // erase current readings
+                        [1,2,3].forEach(function (x) {
+                            app.bullettin['reading' + x] = "";
+                        });
+                        // get the first reading
+                        var oReading1 = getReading(oResponse, "Reading 1");
+                        var oReading2 = getReading(oResponse, "Reading 2");
+                        var oReading3 = getReading(oResponse, "Gospel");
+                        if (!oReading2) {
+                            oReading2 = getReading(oResponse, "Psalm");
+                        }
+
+                        var sReading1 = abbrevReading(oReading1.Citations[0].Reference);
+                        var sReading2 = abbrevReading(oReading2.Citations[0].Reference);
+                        var sReading3 = abbrevReading(oReading3.Citations[0].Reference);
+
+                        app.bullettin.reading1 = sReading1;
+                        app.bullettin.reading2 = sReading2;
+                        app.bullettin.reading3 = sReading3;
+                        alert.show("confirm",
+                            "Updated readings: " + [sReading1, sReading2, sReading3].join(", ")
+                        );
+                    } catch (e) {
+                        throw new Error(e);
+                        alert.show("error", "Error occurred while getting data for the Sunday");
+                    }
+                }, function () {
+                    alert.show("error", "Error occurred while getting data for the Sunday");
+                });
+        },
+        updateTitleFromSunday: function () {
+            var sSelectedSunday = m(app.bullettin.date, "D MMMM YYYY").format("YYYY-MM-DD");
+            getJson('https://www.ewtn.com/se/readings/readingsservice.svc/day/' + sSelectedSunday + '/en')
+                .then(function (oResponse) {
+                    try {
+                        // got data
+                        app.bullettin.title = oResponse.Title;
+                        alert.show("confirm", "Title updated: '" + oResponse.Title + "'");
+                    } catch (e) {
+                        throw new Error(e);
+                        alert.show("error", "Error occurred while getting data for the Sunday");
+                    }
+                    
+                }, function () {
+                    alert.show("error", "Error occurred while getting data for the Sunday");
+                });
+        },
+        updateThemeFromSunday: function () {
+            var sSelectedSunday = m(app.bullettin.date, "D MMMM YYYY").format("YYYY-MM-DD");
+            getJson('https://www.ewtn.com/se/readings/readingsservice.svc/day/' + sSelectedSunday + '/en')
+                .then(function (oResponse) {
+                    try {
+                        // got data
+                        app.setTheme(oResponse.Color);
+                        alert.show("confirm", "Theme was set: " + oResponse.Color);
+                    } catch (e) {
+                        throw new Error(e);
+                        alert.show("error", "Error occurred while getting data for the Sunday");
+                    }
+                    
+                }, function () {
+                    alert.show("error", "Error occurred while getting data for the Sunday");
+                });
         },
         showInput: function (sType, sPathToData, oOptionalObject) {
             function getData(sPath) {
